a33ef34bc12ddf77d8fc49a25496c43e0500050b
Olli Saarinen <osaarine93@gmail.com>
2017-06-10 13:34:23 +0300

    Lisätty sisälttöa ja rakennetta. Me - ja contact osioon etenkin.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 
 function initialize() {
     var mapOptions = {
-        center: new google.maps.LatLng(59.327, 18.067),
+        center: new google.maps.LatLng(61.476, 23.810942),
         zoom: 12,
         scrollwheel: false,
         draggable: true,
@@ -20,32 +20,47 @@ function initialize() {
         '33720 Tampere </span>' +
         '</div>';
     var infowindow = new google.maps.InfoWindow({
-        content: contentString
+        content: contentString,
+        pixelOffset: new google.maps.Size(0, -15)
     });
 
     marker = new google.maps.Marker({
         map: map,
         animation: google.maps.Animation.DROP,
         position: {
-            lat: 59.327,
-            lng: 18.067
+            lat: 61.457693,
+            lng: 23.850942
         }
     });
-    marker.addListener('click', toggleBounce
-        );
-    marker.addListener('click', function() {
-          infowindow.open(map, marker);
-        });
+
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
+        toggleBounce();
+    });
 }
 
 function toggleBounce() {
     if (marker.getAnimation() !== null) {
         marker.setAnimation(null);
-        } else {
+    } else {
         marker.setAnimation(google.maps.Animation.BOUNCE);
 
     }
 }
+
+new Vue({
+  el: '#luonne-lista',
+  data: {
+    items: ['Be sportmaniac','Smile all day everyday','Be curious','Or be awesome instead','Love yourself','Cherish friends']
+  },
+  methods: {
+    shuffle: function () {
+      this.items = _.shuffle(this.items)
+    }
+  }
+})
+
+
 
 $(document).ready(function () {
     initialize();
