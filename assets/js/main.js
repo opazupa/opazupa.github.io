a17d801e58d1e8be696568d08f36de7e162818a0
Olli Saarinen <osaarine93@gmail.com>
2017-07-05 20:19:38 +0300

    Styled Header & intro.

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,7 +110,7 @@ $(document).ready(function () {
         $('a.page-scroll').bind('click', function (event) {
             var $anchor = $(this);
             $('html, body').stop().animate({
-                scrollTop: ($($anchor.attr('href')).offset().top - 50)
+                scrollTop: ($($anchor.attr('href')).offset().top)
             }, 1250, 'easeInOutExpo');
             event.preventDefault();
         });
@@ -135,3 +135,5 @@ $(document).ready(function () {
 
     })(jQuery);
 });
+
+// TODO: animmoitdut sectionit sivuilta tai alhaalta
