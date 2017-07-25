aadb83b91a62c61d964634bd182132985204e429
Olli Saarinen <osaarine93@gmail.com>
2017-07-25 18:34:12 +0300

    Fixed overflows and animation triggers.

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,7 @@ function check_if_in_view() {
     var element_bottom_position = (element_top_position + element_height);
 
     //check to see if this current container is within viewport
-    if ((element_bottom_position >= window_top_position) &&
+    if (((element_bottom_position +10) >= window_top_position) &&
         (element_top_position <= window_bottom_position)) {
       $element.addClass('in-view');
     } else {
